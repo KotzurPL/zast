592ddb66d83e126660e23fc442e4c844ecd88f5b
KotzurPL <kotzur.pl@gmail.com>
2024-02-01 19:57:43 +0100

    Initial commit

---
+++ src/main/java/pl/kotzur/zast/ZastApplication.java
@@ -0,0 +1,13 @@
+package pl.kotzur.zast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kotzur/zast/ZastApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kotzur.zast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
